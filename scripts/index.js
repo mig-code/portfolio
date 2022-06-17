11eaba1b59ba272cd72fc1bfa7fb555094efe095
Miguel <99726342+mig-code@users.noreply.github.com>
2022-06-17 20:36:01 +0200

    Improvements
    not a ready version

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,23 @@
 
 //DATA
 const projects=[{
-    "name":"Estial",
-    "description":"Aplicación para el desarrollo de la estimulación visual"
+    name:"Estial",
+    description:"Aplicación para el desarrollo de la estimulación visual",
+    img:"../images/estial.png",
+    tech:"JavaScript CSS Html Canva "
+    
 },
 {
-    "name":"Crea tu historia",
-    "description":"Aplicación para el desarrollo de la imaginación"
+    name:"Crea tu historia",
+    description:"Aplicación para el desarrollo de la imaginación",
+    img:"../images/crea-tu-historia.png",
+    tech:"JavaScript CSS Html API "
 },
 {
-    "name":"Calculadora",
-    "description":"Descubre cuanto tiempo has estudiado"
+    name:"Calculadora",
+    description:"Descubre cuanto tiempo has estudiado",
+    img:"../images/calculator.png",
+    tech:"JavaScript CSS Html"
 
 }
 ]
@@ -21,7 +28,10 @@ function createProjectsHtml (){
     const projectshtml = projects.map(item=>{
         return `<div class="project">
                        <h4> ${item.name}</h4>
-                        <p>${item.description}</p>
+                       <p>${item.description}</p>
+                       <img src=${item.img}>
+                       <p>${item.tech}</p>
+                        
         </div>`
     })
     return projectshtml.join("")
