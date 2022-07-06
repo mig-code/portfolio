12a768a2f9e85225b82a84b67dbc3088e095ddfe
Miguel <99726342+mig-code@users.noreply.github.com>
2022-07-06 18:36:56 +0200

    Keep improving
    Adding root colors
    Adding animations

--- scripts/index.js
+++ scripts/index.js
@@ -36,11 +36,47 @@ const projects = [
     linkCode: "https://github.com/mig-code/money-test",
     linkLive: "https://mig-code.github.io/money-test/",
   },
+  {
+    name: "Estial",
+    description: "Aplicación para el desarrollo de la estimulación visual",
+    img: "./images/estial.png",
+    tech: "JavaScript CSS Html Canva",
+    whyText:
+      "In fermentum leo sit amet urna sollicitudin, in malesuada tellus laoreet. Quisque enim ex, luctus ut interdum at, iaculis vitae libero. Nam sed mi elit. Maecenas euismod tristique nisi vitae cursus. Aenean a ante scelerisque, blandit magna mollis, interdum nisi. Quisque eget lacinia nisi, eget molestie ex. Mauris tincidunt dui quam, a dictum lacus cursus at. Mauris fringilla ante sed fringilla feugiat. Curabitur ac arcu pellentesque, interdum ex in, porttitor velit. Donec eget sollicitudin erat. Aliquam in arcu tortor. Donec vitae luctus massa. Suspendisse ut condimentum nisl. Phasellus at sollicitudin erat.",
+    howText:
+      "In fermentum leo sit amet urna sollicitudin, in malesuada tellus laoreet. Quisque enim ex, luctus ut interdum at, iaculis vitae libero. Nam sed mi elit. Maecenas euismod tristique nisi vitae cursus. Aenean a ante scelerisque, blandit magna mollis, interdum nisi. Quisque eget lacinia nisi, eget molestie ex. Mauris tincidunt dui quam, a dictum lacus cursus at. Mauris fringilla ante sed fringilla feugiat. Curabitur ac arcu pellentesque, interdum ex in, porttitor velit. Donec eget sollicitudin erat. Aliquam in arcu tortor. Donec vitae luctus massa. Suspendisse ut condimentum nisl. Phasellus at sollicitudin erat.",
+    linkCode: "https://mig-code.github.io/estial/",
+    linkLive: "https://github.com/mig-code/estial",
+  },
+  {
+    name: "Estial",
+    description: "Aplicación para el desarrollo de la estimulación visual",
+    img: "./images/estial.png",
+    tech: "JavaScript CSS Html Canva",
+    whyText:
+      "In fermentum leo sit amet urna sollicitudin, in malesuada tellus laoreet. Quisque enim ex, luctus ut interdum at, iaculis vitae libero. Nam sed mi elit. Maecenas euismod tristique nisi vitae cursus. Aenean a ante scelerisque, blandit magna mollis, interdum nisi. Quisque eget lacinia nisi, eget molestie ex. Mauris tincidunt dui quam, a dictum lacus cursus at. Mauris fringilla ante sed fringilla feugiat. Curabitur ac arcu pellentesque, interdum ex in, porttitor velit. Donec eget sollicitudin erat. Aliquam in arcu tortor. Donec vitae luctus massa. Suspendisse ut condimentum nisl. Phasellus at sollicitudin erat.",
+    howText:
+      "In fermentum leo sit amet urna sollicitudin, in malesuada tellus laoreet. Quisque enim ex, luctus ut interdum at, iaculis vitae libero. Nam sed mi elit. Maecenas euismod tristique nisi vitae cursus. Aenean a ante scelerisque, blandit magna mollis, interdum nisi. Quisque eget lacinia nisi, eget molestie ex. Mauris tincidunt dui quam, a dictum lacus cursus at. Mauris fringilla ante sed fringilla feugiat. Curabitur ac arcu pellentesque, interdum ex in, porttitor velit. Donec eget sollicitudin erat. Aliquam in arcu tortor. Donec vitae luctus massa. Suspendisse ut condimentum nisl. Phasellus at sollicitudin erat.",
+    linkCode: "https://mig-code.github.io/estial/",
+    linkLive: "https://github.com/mig-code/estial",
+  },
+  {
+    name: "Estial",
+    description: "Aplicación para el desarrollo de la estimulación visual",
+    img: "./images/estial.png",
+    tech: "JavaScript CSS Html Canva",
+    whyText:
+      "In fermentum leo sit amet urna sollicitudin, in malesuada tellus laoreet. Quisque enim ex, luctus ut interdum at, iaculis vitae libero. Nam sed mi elit. Maecenas euismod tristique nisi vitae cursus. Aenean a ante scelerisque, blandit magna mollis, interdum nisi. Quisque eget lacinia nisi, eget molestie ex. Mauris tincidunt dui quam, a dictum lacus cursus at. Mauris fringilla ante sed fringilla feugiat. Curabitur ac arcu pellentesque, interdum ex in, porttitor velit. Donec eget sollicitudin erat. Aliquam in arcu tortor. Donec vitae luctus massa. Suspendisse ut condimentum nisl. Phasellus at sollicitudin erat.",
+    howText:
+      "In fermentum leo sit amet urna sollicitudin, in malesuada tellus laoreet. Quisque enim ex, luctus ut interdum at, iaculis vitae libero. Nam sed mi elit. Maecenas euismod tristique nisi vitae cursus. Aenean a ante scelerisque, blandit magna mollis, interdum nisi. Quisque eget lacinia nisi, eget molestie ex. Mauris tincidunt dui quam, a dictum lacus cursus at. Mauris fringilla ante sed fringilla feugiat. Curabitur ac arcu pellentesque, interdum ex in, porttitor velit. Donec eget sollicitudin erat. Aliquam in arcu tortor. Donec vitae luctus massa. Suspendisse ut condimentum nisl. Phasellus at sollicitudin erat.",
+    linkCode: "https://mig-code.github.io/estial/",
+    linkLive: "https://github.com/mig-code/estial",
+  },
 ];
 
 function createProjectsHtml() {
   const projectshtml = projects.map((item) => {
-    return `<div class="project">
+    return `<div class="project reveal">
                        <h4 class="project__title"> ${item.name}</h4>
                        <div class="project__description">
                             <p>${item.description}</p>
@@ -112,3 +148,22 @@ accordionTitle.forEach((eachTitle, i) => {
 });
 
 
+// CSS REVEAL
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 130;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
